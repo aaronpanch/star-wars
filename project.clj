80cef713d228e9a2888e35e6dcfa1c50555cb79b
Aaron Panchal <aaron.panchal@gmail.com>
2018-11-17 17:51:18 -0800

    Update project with better metadata

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject star-wars "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Playing the GraphQL and Clojure"
+  :url "https://github.com/aaronpanch/star-wars"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
